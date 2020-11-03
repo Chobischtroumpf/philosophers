0b38a42d0b3e0205e1b221a59c772fa63155cfd4
Alessandro Dorigo <adorigo@student.s19.be>
2020-11-03 14:28:17 +0100

    oubli de commit

--- philo_one/main.c
+++ philo_one/main.c
@@ -12,18 +12,37 @@
 
 #include "philosophers.h"
 
-void *Philosopher(void *threadid)
+extern int	g_died;
+t_data		g_data;
+
+int		init(t_data *data)
 {
+	data->mutex = \
+		(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t) * data->amount);
+}
+
+int		parse_input(int argc, char **argv, t_data *data)
+{
+	if (argc != 6 && argc != 5)
+		return (0);
+	data->amount = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		data->must_eat_count = ft_atoi(argv[5]);
+	else
+		data->must_eat_count = -1;
 	
 }
 
-int main (int argc, char *argv[])
+int		main (int argc, char *argv[])
 {
-	if (argc < 5 || argc > 6)
-	{
-		ft_putstr_fd("Error: wrong number of arguments\n", 2);
-		return(1);
-	}
-	if (initcontext(argc, argv))
-	
+	pthread_t	*threads;
+	t_philo		*philos;
+
+	if (!parse_input(argc, argv, &g_data))
+		return (error_ret("Argument Error\n"));
+	else if (init(&g_data) == -1)
+
 }
--- philo_one/philosophers.h
+++ philo_one/philosophers.h
@@ -20,20 +20,31 @@
 
 # define NUM_THREADS	8
 
-typedef struct	s_philosopher
+typedef struct	s_data
 {
-	int			amount;
-	uint64_t	time_to_die;
-	uint64_t	time_to_eat;
-	uint64_t	time_to_sleep;
-	int			must_eat_count;
+	int				amount;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				must_eat_count;
+	int				philo_idx;
+	int				n_eating;
+	int				max_eating;
 
-	uint64_t	start;
-}				t_philo;
+	uint64_t		start;
+	pthread_mutex_t	*mutex;
+	pthread_mutex_t	dead;
+	pthread_mutex_t pickup;
+}				t_data;
 
-typedef struct	s_context
+typedef struct			s_philo
 {
-	
-}
+	pthread_mutex_t		*left;
+	pthread_mutex_t		*right;
+	int					idx;
+	int					is_stopped;
+	int					count;
+	uint64_t			last_eat_time;
+}						t_philo;
 
 #endif
