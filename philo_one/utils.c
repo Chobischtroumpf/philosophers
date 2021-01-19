cd3ff42ad6dfcc02b8aed44695ace036b949bb79
Alessandro Dorigo <adorgo@student.s19.be>
2021-01-19 11:07:25 +0100

    testing this

--- philo_one/utils.c
+++ philo_one/utils.c
@@ -6,7 +6,7 @@
 /*   By: adorigo <adorigo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/11 10:17:25 by adorigo           #+#    #+#             */
-/*   Updated: 2021/01/18 16:49:15 by adorigo          ###   ########.fr       */
+/*   Updated: 2021/01/19 11:01:00 by adorigo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ void			ft_putstr_fd(char *str, int fd)
 
 void			ft_usleep(unsigned long sleep_time)
 {
-	usleep(sleep_time * 200);
-	usleep(sleep_time * 200);
-	usleep(sleep_time * 200);
-	usleep(sleep_time * 200);
-	usleep(sleep_time * 200);
+	unsigned long end;
+
+	end = get_time() + sleep_time;
+	while (get_time() < end)
+		usleep(400);
 }
 
 void			ft_free_mutex(t_context *cxt)
